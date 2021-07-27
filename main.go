a3481843ad3273bc77cc28da20d9013baae1cd1b
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2021-07-27 12:22:16 +0100

    add function hook
    Change-Id: I4d0ead83ce47e00c46ec9c3b9fef27341c41ded8

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
-package main
+// Package p contains an HTTP Cloud Function.
+package p
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("vim-go")
+// HelloWorld prints the JSON encoded "message" field in the body
+// of the request or "Hello, World!" if there isn't one.
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	log.Printf("%s, %s\n", b, err)
+	w.WriteHeader(http.StatusOK)
 }
